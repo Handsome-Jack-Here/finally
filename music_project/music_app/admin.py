b875a4318c8ad974631ba60300c6408f4b71a661
Handsome-Jack-Here <whitefrostswift@gmail.com>
2022-10-12 10:15:00 +0300

    купшыеукregister admin for Album

--- music_project/music_app/admin.py
+++ music_project/music_app/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from .models import Band
+from .models import Band, Album
 
 
 @admin.register(Band)
 class BandAdmin(admin.ModelAdmin):
     list_display = ['band_name', 'founded', 'genre', 'slug']
     prepopulated_fields = {'slug': ('band_name',)}
+
+
+@admin.register(Album)
+class AlbumAdmin(admin.ModelAdmin):
+    list_display = ['title', 'release_date']
